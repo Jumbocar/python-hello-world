8d257da81f797099ced550980611973e2f4dac45
jumbocar <jacobblake1112@yahoo.com>
2024-08-07 16:37:30 -0500

    First push

--- hello.py
+++ hello.py
@@ -4,10 +4,22 @@
 # to the console (the panel on the right)
 
 print("Hello");
-
 # Next, PLAY!
 # Modify your code on line 7 to try to accomplish the tasks listed below:
+print('Hello, Jim!');
+#print'Hello World'
+#print (hello world)
+print(2+3);
+print('hello world');
+print('Same message');  print(2)
+print('\"get bent\"')
 
+print("type 1 or 2")
+
+if int(input()) == 1:
+    print('you typed 1')
+else:
+    print('you typed 2')
 #   1. Change the message that is printed.
 #   2. Figure out what the parentheses do. Will the code work without them?
 #   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
@@ -17,3 +29,4 @@ print("Hello");
 #   7. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
 #   8. Other. You choose!
 #
+
